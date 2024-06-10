7301bd161553bb440fa8cca30f002cef1325e5b4
mindbossss <ddrdkg@gmail.com>
2024-06-10 18:16:51 +0530

    push

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href }}"
+    class="rounded-md border border-slate-300 bg-white  font px-2.5 py-1 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-100">
+    {{$slot}}
+</a>
