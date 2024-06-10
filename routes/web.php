7bb834e13563b5217be62b163b10b2838a865362
mindbossss <ddrdkg@gmail.com>
2024-06-10 16:06:15 +0530

    push

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
+Route::get('', fn() => to_route('jobs.index'));
+
 Route::resource('jobs', JobController::class)->only(['index']);
