809058767bf883507b0e7f8463de060fd3aaa133
mindbossss <ddrdkg@gmail.com>
2024-06-10 13:51:09 +0530

    z

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,32 @@
 <x-layout>
     @foreach ($Jobs as $job )
-    <div>{{ $job->title}}</div>
+    <x-card class="mb-4">
+        <div class="flex mb-4 justify-between">
+            <h2 class="text-lg font-medium">
+                {{ $job->title}}
+            </h2>
+            <div class="text-slate-500">{{ $job->salary}}</div>
+        </div>
+
+        <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
+            <div class="flex space-x-4">
+                <div>Company Name</div>
+                <div>{{ $job->location }}</div>
+            </div>
+            <div class="flex space-x-1 text-xs">
+                <x-tag>
+                    {{ Str::ucfirst($job->experience) }}
+                </x-tag>
+                <x-tag>{{ $job->category }}</x-tag>
+            </div>
+        </div>
+
+
+
+        <p class="mb-4 text-sm text-slate-500">
+            {!! nl2br(e($job->description)) !!}
+        </p>
+
+    </x-card>
     @endforeach
 </x-layout>
