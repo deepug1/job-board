13312ecb60920c3859935230399e496b062e8f2f
Deepu Gupta <82046692+deepug1@users.noreply.github.com>
2024-07-01 16:30:35 +0530

    push

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\AuthController;
 
 Route::get('', fn() => to_route('jobs.index'));
@@ -11,3 +12,7 @@ Route::get('login', fn() => to_route('auth.create'))->name('login');
 Route::resource('auth', AuthController::class)->only(['create', 'store']);
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+Route::middleware('auth')->group(function () {
+    Route::resource('job.application', JobApplicationController::class)
+        ->only(['create', 'store']);
+});
